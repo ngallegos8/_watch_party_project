6f81f1d411529ec01a97d212c142ef6905ea065f
Jeffrey <jefbfitzp33@outlook.com>
2024-03-07 11:25:05 -0700

    Vender page complete

--- server/seed.py
+++ server/seed.py
@@ -23,7 +23,7 @@
 #     db.session.commit()
 
 from app import app 
-from models import db, User, Event
+from models import db, User, Event, Venue
 from faker import Faker
 from random import randint
 from datetime import datetime
@@ -33,6 +33,9 @@ faker = Faker()
 with app.app_context():
     print("Deleting Customers")
     User.query.delete()
+    Event.query.delete()
+    Venue.query.delete()
+
 
     stephen = User(username="Stephen", password="password")
     lucy = User( username="lucy", password="dogbone")
@@ -42,12 +45,25 @@ with app.app_context():
     date_time_obj = datetime.datetime(*map(int, date_time_str.split(", ")))
 
     boxingMatch = Event(name ="boxing match", date_time=date_time_obj, description="a boxing match", attending_count="5")
+    tennisMatch = Event(name ="tennis match", date_time=date_time_obj, description="a tennis match", attending_count="4")
+    squashMatch = Event(name ="squash match", date_time=date_time_obj, description="a squash battle", attending_count="10")
     
+    fillmore_venue = Venue(username="Fillmore", password="fillmore", venue_name ="The Fillmore", location="1234 colfax")
+    gothic_venue = Venue(username="Gothic", password="gothic", venue_name ="The Gothic", location="1234 broadway")
+    marquis_venue = Venue(username="Marquis", password="marquis", venue_name ="The Marquis", location="1234 larimer")
+
+
     db.session.add(stephen)
     db.session.add(lucy)
     db.session.add(taki)
 
     db.session.add(boxingMatch)
+    db.session.add(tennisMatch)
+    db.session.add(squashMatch)
+
+    db.session.add(fillmore_venue)
+    db.session.add(gothic_venue)
+    db.session.add(marquis_venue)
     db.session.commit()
 
 
